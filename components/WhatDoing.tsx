a1b61ab6ce1a5057ae9eb19790ece508fec22184
dntandan <deepnarayan006@gmail.com>
2021-04-28 23:25:17 +0530

    hobbies , pills and whatDoing components added

---
+++ components/WhatDoing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function WhatDoing(props) {
+  return (
+    <>
+      <div className="h-auto px-4 py-6 gap-2 flex flex-col items-center rounded-lg border-2 border-dashed">
+        {props.children}
+        <h3 className="lg:text-lg font-semibold">{props.title} </h3>
+        <p className="text-center">{props.desc}</p>
+      </div>
+    </>
+  );
+}
