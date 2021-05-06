1cd53c2025f739445a0084d2485e0c190b7fb339
dntandan <deepnarayan006@gmail.com>
2021-05-06 20:53:47 +0530

    page scrollbar component mounted in layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,18 @@
-import Head from 'next/head';
 import React from 'react';
 import Header from '@/components/Header';
-import Pills from '@/components/Aside';
 import Aside from '@/components/Aside';
+import PageScrollBar from '@/components/PageScrollBar';
 
 const Layout = (props) => (
   <>
     <div className="bg-white text-black dark:bg-gray-900">
       <Header />
+      <PageScrollBar
+        color="#0aa863"
+        gradientColor="#2bf29c"
+        gradient={true}
+        height={4}
+      />
       <div className="lg:px-12 xl:px-48 flex flex-col lg:flex-row">
         <Aside />
         <main className="flex-1 p-3 mt-3 lg:px-8 lg:pb-8 lg:pt-4 h-auto w-full ">
